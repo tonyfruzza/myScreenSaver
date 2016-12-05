eaf6e89557e7a3166840f9b90ee244337a09c10e
tonyfruzza <anthony.fruzza@sturdynetworks.com>
2016-12-04 21:42:33 -0800

    Initial Commit

---
+++ ScreenTest/ScreenTestView.h
@@ -0,0 +1,13 @@
+//
+//  ScreenTestView.h
+//  ScreenTest
+//
+//  Created by Anthony Fruzza on 12/4/16.
+//  Copyright © 2016 Anthony Fruzza. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ScreenTestView : ScreenSaverView
+
+@end
---
+++ ScreenTest/ScreenTestView.m
@@ -0,0 +1,52 @@
+//
+//  ScreenTestView.m
+//  ScreenTest
+//
+//  Created by Anthony Fruzza on 12/4/16.
+//  Copyright © 2016 Anthony Fruzza. All rights reserved.
+//
+
+#import "ScreenTestView.h"
+
+@implementation ScreenTestView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
